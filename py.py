ba7f0da928aa151a6e03c462b07dc04785e22577
Pillarofcats <jacob.white.07@gmail.com>
2024-01-16 14:45:13 -0600

    Update

--- py.py
+++ py.py
@@ -1,104 +1,104 @@
-# #IMMUTABLE TYPES (copies are NOT referenced, copies don't affect real copy)
-# from immutableTypesModule import immutableTypes
-# immutableTypes()
+#IMMUTABLE TYPES (copies are NOT referenced, copies don't affect real copy)
+from immutableTypesModule import immutableTypes
+immutableTypes()
 
-# #MUTABLE TYPES (copies are referenced, copies affect real copy)
-# from mutableTypesModule import mutableTypes
-# mutableTypes()
+#MUTABLE TYPES (copies are referenced, copies affect real copy)
+from mutableTypesModule import mutableTypes
+mutableTypes()
 
-# #SHALLOW COPY
-# from shallowCopyModule import shallowCopy
-# shallowCopy()
+#SHALLOW COPY
+from shallowCopyModule import shallowCopy
+shallowCopy()
 
-# #LIST COMPREHENSION
-# from listComprehensionModule import listComprehension
-# listComprehension()
+#LIST COMPREHENSION
+from listComprehensionModule import listComprehension
+listComprehension()
 
-# #PARAMATER/ARGUMENT/DEFAULT
-# # *args / **kwargs
-# from paramArgsDefaultModule import paramArgsDefault
-# paramArgsDefault()
+#PARAMATER/ARGUMENT/DEFAULT
+# *args / **kwargs
+from paramArgsDefaultModule import paramArgsDefault
+paramArgsDefault()
 
-# #IMPORT MODULES
-# print("|| IMPORT MODULES ||\n")
-# from importAddModule import importAdd
-# #from (module file) import (module function/variables/etc)
-# print(f"from importAddModule import importAdd] \nAdding 1 + 2 with ImportAddModule Fn importAdd(1,2): { importAdd(1,2) }")
+#IMPORT MODULES
+print("|| IMPORT MODULES ||\n")
+from importAddModule import importAdd
+#from (module file) import (module function/variables/etc)
+print(f"from importAddModule import importAdd] \nAdding 1 + 2 with ImportAddModule Fn importAdd(1,2): { importAdd(1,2) }")
 
-# print("")
+print("")
 
-# #GLOBAL INTERPRETER LOCK (GIL) PYTHON ONLY RUNS 1 THREAD AT A TIME
-# #NOT MULTITHREADED
-# from gilModule import gil
-# gil()
+#GLOBAL INTERPRETER LOCK (GIL) PYTHON ONLY RUNS 1 THREAD AT A TIME
+#NOT MULTITHREADED
+from gilModule import gil
+gil()
 
-# #TRY/EXCEPTION
-# from tryExceptionModule import tryException
-# tryException()
+#TRY/EXCEPTION
+from tryExceptionModule import tryException
+tryException()
 
-# #IMPORT RANDOM
-# from randomCardModule import randomCard
-# randomCard()
+#IMPORT RANDOM
+from randomCardModule import randomCard
+randomCard()
 
-# #TUPLE METHODS
-# from tupleMethodsModule import tupleMethods
-# tupleMethods()
+#TUPLE METHODS
+from tupleMethodsModule import tupleMethods
+tupleMethods()
 
-# #LIST METHODS
-# from listMethodsModule import listMethods
-# listMethods()
+#LIST METHODS
+from listMethodsModule import listMethods
+listMethods()
 
-# #SET METHODS
-# from setMethodsModule import setMethods
-# setMethods()
+#SET METHODS
+from setMethodsModule import setMethods
+setMethods()
 
-# #DICT METHODS
-# from dictMethodsModule import dictMethods
-# dictMethods()
+#DICT METHODS
+from dictMethodsModule import dictMethods
+dictMethods()
 
-# #FOR LOOPS
-# from forLoopsModule import forLoops
-# forLoops()
+#FOR LOOPS
+from forLoopsModule import forLoops
+forLoops()
 
-# #CLASSES
-# from classesModule import classes
-# classes()
+#CLASSES
+from classesModule import classes
+classes()
 
-# #ITERATORS
-# from iteratorsModule import iterators
-# iterators()
+#ITERATORS
+from iteratorsModule import iterators
+iterators()
 
-# #POLYMORPHISM
-# from polymorphismModule import polymorphism
-# polymorphism()
+#POLYMORPHISM
+from polymorphismModule import polymorphism
+polymorphism()
 
-# #DATES
-# from datesModule import dates
-# dates()
+#DATES
+from datesModule import dates
+dates()
 
-# #PARSE JSON
-# from parseJSONModule import parseJSON
-# parseJSON()
+#PARSE JSON
+from parseJSONModule import parseJSON
+parseJSON()
 
-# #PIP PACKAGE MANAGER
-# from pipModule import pip
-# pip()
+#PIP PACKAGE MANAGER
+from pipModule import pip
+pip()
 
-# #USER INPUT
-# from userInputModule import userInput
-# userInput()
+#USER INPUT
+from userInputModule import userInput
+userInput()
 
-# #WRITE FILES / READ FILES
-# from writeReadFilesModule import writeReadFiles
-# writeReadFiles()
+#WRITE FILES / READ FILES
+from writeReadFilesModule import writeReadFiles
+writeReadFiles()
 
-# #CLASS MAGIC METHODS
-# from classMagicMethodsModule import classMagicMethods
-# classMagicMethods()
+#CLASS MAGIC METHODS
+from classMagicMethodsModule import classMagicMethods
+classMagicMethods()
 
-# #IMPORT TIMEIT
-# from importTimeItModule import importTimeIt
-# importTimeIt()
+#IMPORT TIMEIT
+from importTimeItModule import importTimeIt
+importTimeIt()
 
 #SOMETHING
 print("|| SOMETHING ||\n")
